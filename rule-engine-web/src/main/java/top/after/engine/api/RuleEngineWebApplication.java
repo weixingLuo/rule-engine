acf2c8077052245346254b5ca027ef0c00b1158a
weixing_luo <weixing_luo@crfchina.com>
2019-04-29 14:39:15 +0800

    修改包结构

---
+++ rule-engine-web/src/main/java/top/after/engine/api/RuleEngineWebApplication.java
@@ -0,0 +1,13 @@
+package top.after.engine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEngineWebApplication.class, args);
+	}
+
+}
